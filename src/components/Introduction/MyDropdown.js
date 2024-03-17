3ec7804a648b144ba1e157e947774b332e7e988c
‘lihsinhuei’ <lihsinhuei@gmail.com>
2024-03-17 14:28:53 -0700

    user can select the  number of quizs and the maximum number of digits

--- src/components/Introduction/MyDropdown.js
+++ src/components/Introduction/MyDropdown.js
@@ -8,24 +8,9 @@ const MyDropdown = (props)=>{
 
     return(
         <div>
-            {/* <select onChange={props.handleSelect}> 
-                {options.map((data)=>(
-                    <option title={data.value} key={data.lable}>{data.value}</option>
-                ))}
-            </select> */}
             <Select onChange={props.handleSelect} options={props.options} defaultValue={props.options[0]} />
-                {/* <DropdownButton id="dropdown-basic-button" title={props.dropdownTitle}>
-                {listArray.map(data=>(
-                     <Dropdown.Item title={data} onChange={this.handleSelect}>{data}</Dropdown.Item>
-                ))}
-                </DropdownButton> */}
-
         </div>
     );
-
-
 }
-
-
 
 export default MyDropdown;
